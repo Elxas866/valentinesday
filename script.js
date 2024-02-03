29e54675079e22e6f5ba7b1cb33b857c6024d57c
Elias Kelta <eliaskelta1@gmail.com>
2024-02-03 11:47:24 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let options = [
+    'Really?',
+    'R u sure?',
+    'Maybe change your mind?',
+    'Think again!',
+    'U missklicked, right?',
+    'Last chance!',
+    'Reconsider?',
+    "Don't be so mean :("
+];
+
+let yesbtn = document.getElementById('yesbtn');
+let nobtn = document.getElementById('nobtn');
+let mult = 1.4;
+let i = 0;
+
+function no() {
+    console.log('No?');
+    // yesbtn
+    yesbtn.style.transform = 'scale(' + mult + ')';
+    mult += 0.4;
+
+    //nobtn
+    nobtn.innerText = options[i];
+    if (i == options.length-1) {
+        i = 0;
+    }else {
+        i++;
+    }
+}
